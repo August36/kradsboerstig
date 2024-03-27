a92bfff1f13c1a04b9adf58e104a9bc2ea38ceee
August <augustjp@live.dk>
2024-03-27 12:24:05 +0100

    burger

---
+++ script.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(navlink => navlink.
+addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
