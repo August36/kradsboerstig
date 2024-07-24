853cfdeaf60f060f00d71ee771749722abc30146
August <augustjp@live.dk>
2024-07-24 12:21:53 +0200

    border buttom and margin

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -1,11 +1,13 @@
 const NewsArticle = ({ image, title, content }) => {
   return (
+    <div className="border-b-4 mb-8 mt-8">
     <div className="flex flex-col md:flex-row mb-8">
       <img src={image} alt={title} className="w-full md:w-1/2 h-48 md:h-auto object-cover" />
       <div className="p-4 md:w-1/2">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p>{content}</p>
       </div>
+    </div>
     </div>
   );
 };
