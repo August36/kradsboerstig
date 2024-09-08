70825032080be5f8f4fe0cc7ea8314d277fbe8e6
August <augustjp@live.dk>
2024-09-08 18:30:57 +0200

    attempt to fix modal problem

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ isOpen, images, modalImageIndex, onClose, onPrev, onNext }) => 
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-100"
+      className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-[9999]" // Increased z-index to ensure it is always on top
       onClick={onClose}
       role="dialog"
       aria-modal="true"
@@ -14,7 +14,7 @@ const Modal = ({ isOpen, images, modalImageIndex, onClose, onPrev, onNext }) => 
       <div className="relative flex items-center justify-center w-full h-full">
         {/* Left Arrow */}
         <button
-          className="absolute left-2 sm:left-4 top-1/2 transform -translate-y-1/2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-50"
+          className="absolute left-2 sm:left-4 top-1/2 transform -translate-y-1/2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-[10000]"
           onClick={(e) => {
             e.stopPropagation();
             onPrev();
@@ -25,19 +25,19 @@ const Modal = ({ isOpen, images, modalImageIndex, onClose, onPrev, onNext }) => 
         </button>
 
         {/* Image with Close Button */}
-        <div className="relative flex justify-center items-center">
+        <div className="relative flex justify-center items-center z-[10000]">
           <h2 id="modal-title" className="sr-only">
             Image Viewer
           </h2>
           <img
             src={images[modalImageIndex].src}
             alt={images[modalImageIndex].title}
-            className="max-w-full max-h-full object-contain xs:max-w-[90vw] xs:max-h-[90vh] sm:max-w-[80vw] sm:max-h-[80vh] md:max-w-[70vw] md:max-h-[70vh] lg:max-w-[60vw] lg:max-h-[60vh] z-20"
+            className="max-w-full max-h-full object-contain xs:max-w-[90vw] xs:max-h-[90vh] sm:max-w-[80vw] sm:max-h-[80vh] md:max-w-[70vw] md:max-h-[70vh] lg:max-w-[60vw] lg:max-h-[60vh]"
             onClick={(e) => e.stopPropagation()} // Prevent closing modal when clicking the image
           />
           {/* Close Button */}
           <button
-            className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-50"
+            className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-[10000]"
             onClick={onClose}
             aria-label="Close Modal"
           >
@@ -47,7 +47,7 @@ const Modal = ({ isOpen, images, modalImageIndex, onClose, onPrev, onNext }) => 
 
         {/* Right Arrow */}
         <button
-          className="absolute right-2 sm:right-4 top-1/2 transform -translate-y-1/2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-50"
+          className="absolute right-2 sm:right-4 top-1/2 transform -translate-y-1/2 text-white bg-gray-800 hover:bg-gray-700 rounded-full p-2 focus:outline-none z-[10000]"
           onClick={(e) => {
             e.stopPropagation();
             onNext();
@@ -60,6 +60,7 @@ const Modal = ({ isOpen, images, modalImageIndex, onClose, onPrev, onNext }) => 
     </div>
   );
 };
+
 
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,37 +33,84 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-800 p-4 z-50 flex items-center justify-between">
-      {/* Home Logo */}
-      <div className="text-white z-50	">
-        <Link to="/" className="text-white hover:text-gray-300">
-          <img
-            className="w-52 h-20"
-            src="Underskrift-logo_white.png"
-            alt="Signature"
-          />
+    {/* Home Logo */}
+    <div className="text-white z-50">
+      <Link to="/" className="text-white hover:text-gray-300">
+        <img
+          className="w-52 h-20"
+          src="Underskrift-logo_white.png"
+          alt="Signature"
+        />
+      </Link>
+    </div>
+  
+    {/* Desktop Links */}
+    <div className="hidden lg:flex space-x-6">
+      {navLinks.map((link) => (
+        <Link
+          key={link.to}
+          to={link.to}
+          className="text-gray-300 hover:text-white text-xl"
+        >
+          {link.label}
         </Link>
-      </div>
-
-      {/* Desktop Links */}
-      <div className="hidden lg:flex space-x-6">
-        {navLinks.map((link) => (
-          <Link
-            key={link.to}
-            to={link.to}
-            className="text-gray-300 hover:text-white text-xl"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </div>
-
-      {/* Burger Button for Mobile */}
-      <button
-        onClick={toggleMenu}
-        className="lg:hidden text-white focus:outline-none"
-        aria-label="Toggle menu"
-      >
-        {isOpen ? (
+      ))}
+    </div>
+  
+    {/* Burger Button for Mobile */}
+    <button
+      onClick={toggleMenu}
+      className="lg:hidden text-white focus:outline-none"
+      aria-label="Toggle menu"
+    >
+      {isOpen ? (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      ) : (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      )}
+    </button>
+  
+    {/* Mobile Menu */}
+    <div
+      ref={menuRef}
+      className={`lg:hidden fixed top-0 left-0 w-full bg-gray-800 text-white transform ${
+        isOpen ? "translate-y-0" : "hidden"
+      } transition-transform duration-300 ease-in-out z-40`}
+    >
+      {/* Mobile Menu Header */}
+      <div className="relative flex justify-end items-center p-4">
+        {/* Close Button */}
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none mr-4"
+          aria-label="Close menu"
+        >
           <svg
             className="w-6 h-6"
             fill="none"
@@ -78,72 +125,24 @@ const Navbar = () => {
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
-        ) : (
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+        </button>
+      </div>
+  
+      {/* Mobile Links */}
+      <div className="flex flex-col items-end space-y-4 mt-4 pr-6 pb-4">
+        {navLinks.map((link) => (
+          <Link
+            key={link.to}
+            to={link.to}
+            className="text-xl py-2 hover:bg-gray-700 text-right pr-4"
+            onClick={toggleMenu}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        )}
-      </button>
-
-{/* Mobile Menu */}
-<div
-  ref={menuRef}
-  className={`lg:hidden fixed top-0 left-0 w-full bg-gray-800 text-white transform ${
-    isOpen ? "translate-y-0" : "hidden"
-  } transition-transform duration-300 ease-in-out z-40`} // Removed overflow-y-auto
->
-  {/* Mobile Menu Header */}
-  <div className="relative flex justify-end items-center p-4">
-    {/* Close Button */}
-    <button
-      onClick={toggleMenu}
-      className="text-white focus:outline-none mr-4"
-      aria-label="Close menu"
-    >
-      <svg
-        className="w-6 h-6"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
-    </button>
-  </div>
-
-  {/* Mobile Links */}
-  <div className="flex flex-col items-end space-y-4 mt-4 pr-6 pb-4">
-    {navLinks.map((link) => (
-      <Link
-        key={link.to}
-        to={link.to}
-        className="text-xl py-2 hover:bg-gray-700 text-right pr-4"
-        onClick={toggleMenu}
-      >
-        {link.label}
-      </Link>
-    ))}
-  </div>
-</div>
-
-    </nav>
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  </nav>
   );
 };
 
