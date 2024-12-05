224d1b370d7b4e2a8fe836ace4245ce552020f4d
August <augustjp@live.dk>
2024-12-05 15:38:57 +0100

    responsive hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,22 @@
-import PropTypes from "prop-types";
-
-const Hero = ({ image }) => {
+const Hero = () => {
   return (
-    <div
-      className="relative w-full h-screen bg-center bg-no-repeat bg-cover mb-20" // Full-height hero
-      style={{ backgroundImage: `url(${image})` }}
-    >
+    <div className="relative w-full" style={{ minHeight: "calc(100vh - 112px)" }}>
+      <picture>
+        <source srcSet="/HeroImgs/Hero-desktop-xl.png" media="(min-width: 1624px)" />
+        <source srcSet="/HeroImgs/Hero-desktop.png" media="(min-width: 1024px)" />
+        <source srcSet="/HeroImgs/Hero-tablet.png" media="(min-width: 434px)" />
+        <source srcSet="/HeroImgs/Hero-tablet-small.png" media="(min-width: 640px)" />
+        <img
+          src="/HeroImgs/Hero-mobile.png"
+          alt="Hero"
+          className="mx-auto"
+          loading="lazy"
+        />
+      </picture>
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-0" />
-      
-      
-      {/* <div className="relative z-10 flex items-center justify-center h-full">
-        <div className="bg-white bg-opacity-80 text-white p-6 rounded-md flex flex-col items-center justify-center mb-60 max-w-4xl mx-auto">
-          <h1 className="text-gray-500 text-4xl sm:text-5xl md:text-6xl font-bold drop-shadow-lg">
-            Krasbørstig kunst
-          </h1>
-          <h2 className="text-gray-500 text-xl sm:text-2xl md:text-3xl mt-4 drop-shadow-lg">
-            Malerier af Kell Jarner
-          </h2>
-        </div>
-      </div> */}
+      <div className="absolute inset-0 bg-black opacity-30"></div>
     </div>
   );
-};
-
-Hero.propTypes = {
-  image: PropTypes.string.isRequired,
 };
 
 export default Hero;
